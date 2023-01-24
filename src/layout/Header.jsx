b4ec0e1666c5ea97f72bc6773345b31695ba53b8
wundervaffetun <egor.kor013000@gmail.com>
2023-01-24 15:45:08 +0300

    gh-pages

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
                 </a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li>
-                        <a href="https://github.com/wunderwaffetun">Repo</a>
+                        <a href="https://github.com/wunderwaffetun/react-shop.git" target={"_blank"} rel="noreferrer">Repo</a>
                     </li>
                 </ul>
             </div>
